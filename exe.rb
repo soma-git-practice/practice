5d747f81e596d3043ef2cd8e88c4d79cb65a0538
soma <somofgit@gmail.com>
2025-04-29 08:33:08 +0900

    用意

--- exe.rb
+++ exe.rb
@@ -5,24 +5,7 @@ get '/' do
   erb :index
 end
 
-get '/api', provides: 'json' do
-  data = [
-    {
-      team: 'A',
-      member: [
-        { name: '田中', age: 25, sex: '男' },
-        { name: '佐藤', age: 42, sex: '男' },
-        { name: '鈴木', age: 32, sex: '女' },
-      ]
-    },
-    {
-      team: 'B',
-      member: [
-        { name: '山本', age: 40, sex: '女' },
-        { name: '伊東', age: 35, sex: '男' },
-        { name: '山田', age: 28, sex: '女' },
-      ]
-    }
-  ]
+get '/item', provides: 'json' do
+  data = { name: '太郎' }
   data.to_json
 end
